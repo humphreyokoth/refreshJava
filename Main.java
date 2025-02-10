f8b770eb77ee0fe9f909c45da5bbd53f49c46442
humphreyokoth <humpasp@gmail.com>
2025-02-10 16:05:34 +0300

    Update greeting message in Main class to reflect learning focus

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World i am refreshing my mind on the java basics");
     }
 }
